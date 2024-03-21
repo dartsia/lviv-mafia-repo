b38d4044c0176699d8323ed200bb6d5cf8061c97
Maks Sushchuk <maks313455@gmail.com>
2024-03-21 18:17:24 +0200

    many changes

--- volonterio/server/Routes/web.js
+++ volonterio/server/Routes/web.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const app = express();
 
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
 
 let AuthController = require('../Controllers/AuthController'); 
 
+router.post(
+  '/registry',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+  ], 
+  AuthController.register
+);
 
-router.post('/registry', [
-  body('email').isEmail().withMessage('Email must be valid'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-], AuthController.register);
-
-router.post('/login', [
-  body('email').isEmail().withMessage('Email must be valid'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-],  AuthController.loginStore);
+router.post(
+  '/login', 
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+  ],  
+  AuthController.loginStore
+);
 
 module.exports = router;
