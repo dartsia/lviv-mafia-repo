c825193731a8a248be6e1bdf9593946cd2594936
dartsia <nichisniukd.7b@gmail.com>
2024-03-29 12:48:41 +0200

    added validation for signing up

--- volonterio/server/Controllers/AuthController.js
+++ volonterio/server/Controllers/AuthController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+<<<<<<< Updated upstream
 const User = require('../Models/User');
 
 class AuthController {
@@ -8,16 +9,41 @@ class AuthController {
     }
 
     loginStore(req, res) {
+=======
+
+class AuthController {
+    register(req, res) {
+        const { name, email, password, confirmedPassword, surname } = req.body;
+        // console.log(email, password, name, confirmedPassword); 
+        
+>>>>>>> Stashed changes
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        res.send(`Signed up with email: ${email}`);
+    }
+
+    loginStore(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
         const { email, password } = req.body;
+<<<<<<< Updated upstream
 
         User.create({ name: `${email}`, age: `${password}` });
 
         res.send(`Okey`);
+=======
+        console.log(email, password); 
+
+
+        res.send(`Logged in with email: ${email}`);
+>>>>>>> Stashed changes
     }
 }
 
--- volonterio/server/Routes/web.js
+++ volonterio/server/Routes/web.js
@@ -1,8 +1,12 @@
 const express = require('express');
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 const router = express.Router();
 const { body } = require('express-validator');
 
+<<<<<<< Updated upstream
 let AuthController = require('../Controllers/AuthController'); 
 
 router.post(
@@ -22,5 +26,26 @@ router.post(
   ],  
   AuthController.loginStore
 );
+=======
+const AuthController = require('../Controllers/AuthController');
+const signUpValidation = require('../Validators/AuthValidator/signUpValidator');
+
+router.post('/registry', [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+], (req, res) => {
+  const errors = signUpValidation(req);
+  if (errors.length > 0) {
+    return res.status(400).json({ errors: errors });
+  } else {
+    AuthController.register(req, res);
+  }
+});
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+], AuthController.loginStore);
+>>>>>>> Stashed changes
 
 module.exports = router;
