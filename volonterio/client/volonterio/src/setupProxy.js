2063e3c0764259622e39b8a100b92f5273a3f297
Rewer6 <rewer0ten@gmail.com>
2024-05-15 23:44:42 +0300

    some changes with proxy

--- volonterio/client/volonterio/src/setupProxy.js
+++ volonterio/client/volonterio/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use('/api', createProxyMiddleware({
-    target: 'http://localhost:4000', // адреса зовнішнього сервера
+    target: 'http://localhost:3001', // адреса зовнішнього сервера
     changeOrigin: true,
     pathRewrite: {
       '^/': '', // Якщо запити мають префікс /api, ви можете його вилучити
